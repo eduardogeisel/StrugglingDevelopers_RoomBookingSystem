71a53c7ebf45127c6773143e7fe9ebbc0a76e648
Eduardo Geisel Peres <eduardogeisel@hotmail.com>
2020-10-28 13:10:38 -0700

    Added features for login implementation on DAOImpl class

--- src/main/java/com/csis3275/dao_cwu_18/DAOImpl_cwu_18.java
+++ src/main/java/com/csis3275/dao_cwu_18/DAOImpl_cwu_18.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
 
 import com.csis3275.model_cwu_18.BookingMapper_cwu_18;
 import com.csis3275.model_cwu_18.Booking_cwu_18;
+import com.csis3275.model_cwu_18.LoginMapper_epe_07;
+import com.csis3275.model_cwu_18.Login_epe_07;
 
 
 @Component
@@ -16,6 +18,7 @@ public class DAOImpl_cwu_18 {
 	
 	JdbcTemplate jdbcTemplate;
 	private final String SQL_GET_ALL = "SELECT * FROM BOOKINGS";
+	private final String SQL_GET_EMAIL_PASS = "SELECT EMAIL, PASSWORD FROM USERS";
 	
 	@Autowired
 	public DAOImpl_cwu_18(DataSource dataSource) {
@@ -25,4 +28,10 @@ public class DAOImpl_cwu_18 {
 	public List<Booking_cwu_18> getAllBookings(){
 		return jdbcTemplate.query(SQL_GET_ALL,new BookingMapper_cwu_18());
 	}
+	
+	//Login activity
+	public List<Login_epe_07> getEmailPassord(){
+		return jdbcTemplate.query(SQL_GET_EMAIL_PASS,new LoginMapper_epe_07());
+	}
+	
 }
