b824fac8e0e3af33ada301f768ccdc627a4e3f4d
Eduardo Geisel Peres <eduardogeisel@hotmail.com>
2020-11-13 10:02:09 -0800

    Added more information to javadoc on login feature.

--- src/main/java/com/csis3275/model_cwu_18/Login_epe_07.java
+++ src/main/java/com/csis3275/model_cwu_18/Login_epe_07.java
@@ -3,7 +3,13 @@ package com.csis3275.model_cwu_18;
 /**
  * 
  * @author Eduardo Peres - 300318007
- * This is the login model, with email and password string variables
+ * This is the login model, with email and password string variables.
+ * Once the user opens the webapp, he goes to a login page where credentials such as email and password are required.
+ * If the user does not have an account, there is a link that he gets redirrected to a page to create an account.
+ * After pushing the credentials, the webapp will check with the database if the email and password are correct and will log in the user.
+ * Therer are three types of differente users, student, faculty and admin. Admin gets redirrected to an admin features page while student and faculty
+ * gets redirrected to a booking features page.
+ * If the credentials punched in are incorrect, the user will be prompted with a message telling him that the credentials are wrong, to try again.
  *
  */
 
