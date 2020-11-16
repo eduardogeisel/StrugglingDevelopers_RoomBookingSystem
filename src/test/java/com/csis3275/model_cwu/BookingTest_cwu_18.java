cfc2629d08284df1e06cadafcd8cfe0b986544c3
Chengze Wu <wuc23@student.douglascollege.ca>
2020-11-15 23:42:50 -0800

    Added BookingTest

---
+++ src/test/java/com/csis3275/model_cwu/BookingTest_cwu_18.java
@@ -0,0 +1,14 @@
+package com.csis3275.model_cwu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookingTest_cwu_18 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
