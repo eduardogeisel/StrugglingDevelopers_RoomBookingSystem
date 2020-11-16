2a5dc214d488bcea6ecfb8a86e308f3285b44756
Chengze Wu <wuc23@student.douglascollege.ca>
2020-11-15 23:34:02 -0800

    Ready to push

--- src/main/java/com/csis3275/model_cwu_18/Booking_cwu_18.java
+++ src/main/java/com/csis3275/model_cwu_18/Booking_cwu_18.java
@@ -13,7 +13,6 @@ package com.csis3275.model_cwu_18;
 import java.time.LocalTime;
 import java.util.Date;
 
-
 import org.springframework.format.annotation.DateTimeFormat;
 
 import com.csis3275.controller_cwu_18.IdGenerator_cwu_18;
@@ -28,62 +27,77 @@ public class Booking_cwu_18 {
 	private String description;
 	private int user_id;
 	private String room_id;
-	
-	
+
 	public int getBooking_id() {
 		return booking_id;
 	}
+
 	public void setBooking_id(int booking_id) {
 		this.booking_id = IdGenerator_cwu_18.getRandomBookingID();
 	}
+
 	public LocalTime getStartTime() {
 		return startTime;
 	}
+
 	@DateTimeFormat(pattern = "HH:mm")
 	public void setStartTime(LocalTime startTime) {
 		this.startTime = startTime;
 	}
+
 	public LocalTime getEndTime() {
 		return endTime;
 	}
+
 	@DateTimeFormat(pattern = "HH:mm")
 	public void setEndTime(LocalTime endTime) {
 		this.endTime = endTime;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public int getUser_id() {
 		return user_id;
 	}
+
 	public void setUser_id(int user_id) {
 		this.user_id = user_id;
 	}
+
 	public String getRoom_id() {
 		return room_id;
 	}
+
 	public void setRoom_id(String room_id) {
 		this.room_id = room_id;
 	}
+
 	public Date getDateTime() {
 		return dateTime;
 	}
+
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	public void setDateTime(Date dateTime) {
 		this.dateTime = dateTime;
 	}
+
 	public Booking_cwu_18(int booking_id, Date dateTime, LocalTime startTime, LocalTime endTime, String title,
 			String description, int user_id, String room_id) {
-		
+
 		this.booking_id = booking_id;
 		this.dateTime = dateTime;
 		this.startTime = startTime;
@@ -93,7 +107,8 @@ public class Booking_cwu_18 {
 		this.user_id = user_id;
 		this.room_id = room_id;
 	}
-	public Booking_cwu_18() {}
-	
-	
+
+	public Booking_cwu_18() {
+	}
+
 }
