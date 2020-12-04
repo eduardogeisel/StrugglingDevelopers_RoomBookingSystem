bd4d31e32ad3b9d138b85f93b796c9a06e4235cd
Eduardo Geisel Peres <eduardogeisel@hotmail.com>
2020-12-03 17:24:07 -0800

    Updated controller

--- src/main/java/com/csis3275/controller_cwu_18/ViewRoomPhotosController_epe_07.java
+++ src/main/java/com/csis3275/controller_cwu_18/ViewRoomPhotosController_epe_07.java
@@ -1,17 +1,28 @@
 package com.csis3275.controller_cwu_18;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.csis3275.model_cwu_18.ViewRoomPhotos_epe_07;
+
+
+
 @Controller
+
+@RequestMapping("/roomPhotos")
 
 public class ViewRoomPhotosController_epe_07 {
 	
-	 @RequestMapping(value="/",method = RequestMethod.GET)
-	    public ModelAndView index() {
-	        ModelAndView mv=new ModelAndView("welcome");
-	       return mv;
+	@RequestMapping(method = RequestMethod.GET)
+	public String newProfile(ModelMap model) {
+		ViewRoomPhotos_epe_07 roomPhotos = new ViewRoomPhotos_epe_07();
+		
+		model.addAttribute("roomPhotos", roomPhotos);
+		
+		return "viewRoomPhotos_epe_07";
+	}
 
 }
